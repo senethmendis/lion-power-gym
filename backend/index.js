80e059f227909ae6d80652e983dec3a5350a2e51
senethmendis <mendis.seneth@gmail.com>
2024-11-02 01:56:44 +0530

    initial

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.BACKEND_PORT;
+
+// get main rotes
+const members = require("./routes/members");
+
+// user main routes
+app.use("/members", members);
+
+//test route
+app.get("/helloworld", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log("server is running port" + port);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <h1>Helloworld</h1>
+    </>
+  );
+};
+
+export default App;

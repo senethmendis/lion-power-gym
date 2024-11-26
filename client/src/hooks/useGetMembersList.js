81c1b65387376e992485df94626c59490f021b14
senethmendis <mendis.seneth@gmail.com>
2024-11-27 00:40:26 +0530

    "Refactor Members component to use useGetMemberList hook, removing local state and fetchMemberData function."

---
+++ client/src/hooks/useGetMembersList.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const useGetMemberList = () => {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const res = await axios.get(`${import.meta.env.VITE_BASE_URL}/members`);
+        console.log(res.data);
+        setData(res.data);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return [data, loading];
+};
+
+export default useGetMemberList;
--- client/src/pages/members/Members.jsx
+++ client/src/pages/members/Members.jsx
@@ -6,6 +6,7 @@ import { Pencil } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import useGetMemberList from "@/hooks/useGetMembersList";
 
 const Members = () => {
   const tableHeads = [
@@ -18,64 +19,38 @@ const Members = () => {
     "Acton",
   ];
 
-  const [members, setMembers] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  const fetchMemberData = async () => {
-    try {
-      const res = await axios.get(`${import.meta.env.VITE_BASE_URL}/members`);
-      setLoading(true);
-
-      setMembers(res.data.memberData);
-      setLoading(false);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchMemberData();
-  }, []);
+  const [members, loading] = useGetMemberList();
 
   return (
     <Section>
       <ModulerTable tableHeads={tableHeads} tableTitle={"Members"}>
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          <>
-            {members?.map((member, i) => (
-              <TableRow key={i}>
-                <TableCell className="font-medium">{member.name}</TableCell>
-                <TableCell>
-                  {new Date(member.birthday).toISOString().split("T")[0]}
-                </TableCell>
-                <TableCell>{member.age}</TableCell>
-                <TableCell>{member.phoneNumber}</TableCell>
-                <TableCell>
-                  {new Date(member.registeredDate).toISOString().split("T")[0]}
-                </TableCell>
-                <TableCell>
-                  {member.accessCard ? (
-                    <Badge variant={"outline"} className={"bg-green-600"}>
-                      Yes
-                    </Badge>
-                  ) : (
-                    <Badge variant={"outline"}> No </Badge>
-                  )}
-                </TableCell>
-                <TableCell>
-                  <Link
-                    to={`/members/edit/${member._id}`}
-                    className="bg-red-500"
-                  >
-                    <Pencil size={20} />
-                  </Link>
-                </TableCell>
-              </TableRow>
-            ))}
-          </>
-        )}
+        {members.memberData?.map((member, i) => (
+          <TableRow key={i}>
+            <TableCell className="font-medium">{member.name}</TableCell>
+            <TableCell>
+              {new Date(member.birthday).toISOString().split("T")[0]}
+            </TableCell>
+            <TableCell>{member.age}</TableCell>
+            <TableCell>{member.phoneNumber}</TableCell>
+            <TableCell>
+              {new Date(member.registeredDate).toISOString().split("T")[0]}
+            </TableCell>
+            <TableCell>
+              {member.accessCard ? (
+                <Badge variant={"outline"} className={"bg-green-600"}>
+                  Yes
+                </Badge>
+              ) : (
+                <Badge variant={"outline"}> No </Badge>
+              )}
+            </TableCell>
+            <TableCell>
+              <Link to={`/members/edit/${member._id}`} className="bg-red-500">
+                <Pencil size={20} />
+              </Link>
+            </TableCell>
+          </TableRow>
+        ))}
       </ModulerTable>
     </Section>
   );

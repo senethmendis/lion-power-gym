ba7e1aedd8a85beb262f4e9194eb7aa4e78fc1a9
Seneth Mendis <83528403+senethmendis@users.noreply.github.com>
2024-11-27 17:54:32 +0530

    "Removed test data files, updated useGetMembersList hook to comment out console log, and wrapped App component in StrictMode"

--- client/src/hooks/useGetMemberById.js
+++ client/src/hooks/useGetMemberById.js
@@ -1,18 +1,21 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const useGetMemberList = () => {
-  const [data, setData] = useState({});
+const useGetMemberById = ({ id }) => {
+  const [member, setMember] = useState({});
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         setLoading(true);
-        const res = await axios.get(`${import.meta.env.VITE_BASE_URL}/members`);
+        const res = await axios.get(
+          `${import.meta.env.VITE_BASE_URL}/members/${id}`
+        );
         // console.log(res.data);
-        setData(res.data);
+
         setLoading(false);
+        setMember(res.data);
       } catch (error) {
         console.error(error);
       }
@@ -21,7 +24,6 @@ const useGetMemberList = () => {
     fetchData();
   }, []);
 
-  return [data, loading];
+  return [member, loading];
 };
-
-export default useGetMemberList;
+export default useGetMemberById;
